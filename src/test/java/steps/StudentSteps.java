8f2f57e5647f4c04d906434fbec8be307203efdb
nocetem <matthew.nocete@assurity.co.nz>
2021-07-16 10:56:47 +1200

    Student

--- src/test/java/steps/StudentSteps.java
+++ src/test/java/steps/StudentSteps.java
@@ -3,6 +3,7 @@ package steps;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,22 +16,23 @@ public class StudentSteps {
         // Write code here that turns the phrase above into concrete actions
 //        throw new io.cucumber.java.PendingException();
         RestAssured.baseURI = "https://it-foundations.app.ap.assurity.cloud/";
+        RestAssured.defaultParser = Parser.JSON;
         response = RestAssured.get("people/" + studentId);
         System.out.println(response.asPrettyString());
     }
     @Then("I can see that their name is {string}")
-    public void i_can_see_that_their_name_is(String string) {
+    public void i_can_see_that_their_name_is(String fullName) {
         // Write code here that turns the phrase above into concrete actions
-        assertEquals(string, response.path("firstName") + " " + response.path("lastName"));
+        assertEquals(fullName, response.path("firstName") + " " + response.path("lastName"));
 //        response.asPrettyString()
 
     }
     @Then("they have a {string} from {string}")
-    public void they_have_a_from(String string, String string2) {
+    public void they_have_a_from(String degree, String university) {
         // Write code here that turns the phrase above into concrete actions
 //        throw new io.cucumber.java.PendingException();
-        assertEquals(string, response.path("degree"));
-        assertEquals(string2, response.path("university"));
+        assertEquals(degree, response.path("degree"));
+        assertEquals(university, response.path("university"));
     }
 
 }
